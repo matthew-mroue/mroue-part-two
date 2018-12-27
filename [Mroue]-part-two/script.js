9ea1466174fb4b817bc9144f611253bfd105e0dc
matthew-mroue <46120771+matthew-mroue@users.noreply.github.com>
2018-12-27 02:02:47 -0500

    Add files via upload

--- [Mroue]-part-two/script.js
+++ [Mroue]-part-two/script.js
@@ -1,58 +1,67 @@
 var eventType = window.prompt("Enter the type of event:")
 var tempFahr = window.prompt("Enter the current Temperature:")
-var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear a ___ and ___ "
-if(eventType === "Casual")
+
+if(eventType === "casual")
 {
     if(tempFahr < 54)
     {
         var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear something comfy and a coat."
         document.write(result)
+        console.log(result)
     }
     else if(tempFahr >= 54 && tempFahr <= 70)
     {
         var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear something comfy and a jacket."
         document.write(result)
+        console.log(result)
     }
     else if(tempFahr > 70)
     {
         var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear something comfy and no jacket."
         document.write(result)
+        console.log(result)
     }
         
 }
-else if(eventType === "Semi-formal")
+else if(eventType === "semi-formal")
 {
     if(tempFahr < 54)
     {
         var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear a polo and a coat."
         document.write(result)
+        console.log(result)
     }
     else if(tempFahr >= 54 && tempFahr <= 70)
     {
         var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear a polo and a jacket."
         document.write(result)
+        console.log(result)
     }
     else if(tempFahr > 70)
     {
         var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear a polo and no jacket."
         document.write(result)
+        console.log(result)
     }
 }
-else if(eventType === "Formal")
+else if(eventType === "formal")
 {
     if(tempFahr < 54)
     {
         var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear a suit and a coat."
         document.write(result)
+        console.log(result)
     }
     else if(tempFahr >= 54 && tempFahr <= 70)
     {
         var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear a suit and a jacket."
         document.write(result)
+        console.log(result)
     }
     else if(tempFahr > 70)
     {
         var result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear a suit and no jacket."
         document.write(result)
+        console.log(result)
     }
 }
